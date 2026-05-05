b0f10c1101018572c285263cd681c8f7ade97b70
Styphler17 <styphler17@gmail.com>
2026-05-06 00:06:07 +0200

    perf: setup TanStack Query client

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
 import { ChaptersProvider } from './context/ChaptersContext'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 300000, // 5 minutes
+      retry: 3,
+      networkMode: 'always',
+    },
+  },
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <ChaptersProvider>
-          <App />
-        </ChaptersProvider>
-      </BrowserRouter>
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <ChaptersProvider>
+            <App />
+          </ChaptersProvider>
+        </BrowserRouter>
+      </HelmetProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
